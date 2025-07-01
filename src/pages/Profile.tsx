f019d33687be51e27222632b1b15f06069691f44
vicky0004 <csevicky03@gmail.com>
2025-07-01 14:42:23 +0530

    fix: name of profile value

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -16,8 +16,8 @@ function Profile() {
       <h2>Profile</h2>
       {isInstalled && identity ? (
         <>
-          <p><strong>Name: </strong>{identity.email}</p>
-          <p><strong>Email: </strong> {identity.name}</p>
+          <p><strong>Name: </strong>{identity.name}</p>
+          <p><strong>Email: </strong> {identity.email}</p>
           <p><strong>Web3 Name: </strong> {identity.web3Name}</p>
           <p><strong>DID: </strong> {identity.did}</p>
           {/* <p><strong>Address:</strong> {identity.address}</p> */}
