71f4ff753a0572711b3a26531634576280034e3c
vicky0004 <csevicky03@gmail.com>
2025-07-04 11:55:51 +0530

    added receive module

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { useMiniKit } from 'sdk-sporran-test/minikit-provider';
 import Profile from './pages/Profile';
 import Payment from './pages/Payment';
+import Receive from './pages/Receive';
 
 function App() {
   const { isInstalled } = useMiniKit();
@@ -11,12 +12,14 @@ function App() {
     <Router>
       <h1>Mini-App {isInstalled ? "sdk installed" : "sdk not installed"}</h1>
       <nav style={{ display: 'flex', gap: '1rem', marginBottom: '1rem' }}>
-        <Link to="/profile">Profile</Link>
-        <Link to="/payment">Payment</Link>
+        <Link className='tab_button' id='tab_profile' to="/profile">Profile</Link>
+        <Link className='tab_button' id='tab_payment' to="/payment">Send</Link>
+        <Link className='tab_button' id='tab_receive' to="/receive">Receive</Link>
       </nav>
       <Routes>
         <Route path="/profile" element={<Profile />} />
         <Route path="/payment" element={<Payment />} />
+        <Route path="/receive" element={<Receive />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Receive.tsx
@@ -0,0 +1,29 @@
+import { useMiniKit } from "sdk-sporran-test/minikit-provider";
+import { useEffect } from "react";
+import styles from './Profile.module.css';
+
+function Profile() {
+  const { isInstalled, identity } = useMiniKit();
+
+  useEffect(() => {
+    if (isInstalled) {
+      console.log("MiniKit is installed.");
+      console.log("Identity:", identity);
+    }
+  }, [isInstalled, identity]);
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Receive Credentials</h2>
+      {isInstalled && identity ? (
+        <div className={styles.details}>
+          <p><strong>WalletAddress: </strong>{identity.walletAddress}</p>
+        </div>
+      ) : (
+        <p className={styles.notFound}>Profile not found.</p>
+      )}
+    </div>
+  );
+}
+
+export default Profile;
