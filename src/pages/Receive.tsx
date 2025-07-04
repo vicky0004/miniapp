999c820c805f3230cfeac9792903e7565a338db3
vicky0004 <csevicky03@gmail.com>
2025-07-04 14:36:56 +0530

    Feat: Added the QrCode for receiver wallet address

--- src/pages/Receive.tsx
+++ src/pages/Receive.tsx
@@ -2,6 +2,7 @@ import { useMiniKit } from "sdk-sporran-test/minikit-provider";
 import { useEffect, useState } from "react";
 import { FiCopy, FiCheck } from 'react-icons/fi';
 import styles from './Receive.module.css';
+import { QRCodeCanvas } from 'qrcode.react';
 
 function Profile() {
   const { isInstalled, identity } = useMiniKit();
@@ -36,9 +37,14 @@ function Profile() {
         )}
       </div>
       {isInstalled && identity ? (
-        <div className={styles.details}>
-          <p className={styles.longtext}><strong>WalletAddress: </strong>{identity.walletAddress}</p>
-        </div>
+        <>
+          <div className={styles.details}>
+            <p className={styles.longtext}><strong>WalletAddress: </strong>{identity.walletAddress}</p>
+          </div>
+          <div className={styles.qrWrapper}>
+            <QRCodeCanvas value={identity.walletAddress ? identity.walletAddress : "null"} size={128} marginSize={1} title={"scan to copy"}/>
+          </div>
+        </>
       ) : (
         <p className={styles.notFound}>Profile not found.</p>
       )}
